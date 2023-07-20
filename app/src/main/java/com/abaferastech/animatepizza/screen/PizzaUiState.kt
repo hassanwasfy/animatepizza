9932818f33347357ba9e52ff628c8d025b89c740
abaferas <wasfyelgamed@gmail.com>
2023-07-20 14:52:53 +0300

    implement Pizza screen & ui state

---
+++ app/src/main/java/com/abaferastech/animatepizza/screen/PizzaUiState.kt
@@ -0,0 +1,20 @@
+package com.abaferastech.animatepizza.screen
+
+data class OrderUiState(
+    val pizzaBreads: List<PizzaUiState> = emptyList(),
+    val currentPage: Int = 0,
+)
+
+data class PizzaUiState(
+    val id: Int = 0,
+    val defaultSize: Float = 200f,
+    val image: Int = 0,
+    val pizzaIngredients: List<IngredientsPizzaTypeUiState> = emptyList()
+)
+
+data class IngredientsPizzaTypeUiState(
+    val id: Int = 0,
+    val icon: Int = 0,
+    val image: Int = 0,
+    val isSelected: Boolean = false
+)
