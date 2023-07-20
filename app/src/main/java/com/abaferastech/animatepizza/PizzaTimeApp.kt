d77132ea9fa7e2a3555a3c47d50d0f6c1d0489be
abaferas <wasfyelgamed@gmail.com>
2023-07-20 14:53:11 +0300

    implement Pizza app & view model

---
+++ app/src/main/java/com/abaferastech/animatepizza/PizzaTimeApp.kt
@@ -0,0 +1,7 @@
+package com.abaferastech.animatepizza
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PizzaTimeApp : Application()
---
+++ app/src/main/java/com/abaferastech/animatepizza/screen/PizzaViewModel.kt
@@ -0,0 +1,67 @@
+package com.abaferastech.animatepizza.screen
+
+import androidx.lifecycle.ViewModel
+import com.abaferastech.animatepizza.PizzaData
+import com.abaferastech.animatepizza.screen.PizzaInteractionListener
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import javax.inject.Inject
+
+@HiltViewModel
+class PizzaViewModel @Inject constructor() : ViewModel(), PizzaInteractionListener {
+
+    private val _state = MutableStateFlow(OrderUiState())
+    val state = _state.asStateFlow()
+
+
+    init {
+        _state.update { it.copy(pizzaBreads = PizzaData.pizza()) }
+        _state.update {
+            it.copy(pizzaBreads = it.pizzaBreads.map {
+                it.copy(pizzaIngredients = PizzaData.pizzaIngredients())
+            })
+        }
+    }
+
+    override fun onIngredientsPizzaClick(ingredientPosition: Int, pizzaPosition: Int) {
+        _state.update {
+            it.copy(
+                it.pizzaBreads.mapIndexed { pizzaIndex, pizza ->
+                    if (pizzaIndex == pizzaPosition) {
+                        pizza.copy(
+                            pizzaIngredients = pizza.pizzaIngredients.mapIndexed { index, ingredient ->
+                                if (index == ingredientPosition) {
+                                    ingredient.copy(isSelected = !ingredient.isSelected)
+                                } else {
+                                    ingredient.copy(isSelected = ingredient.isSelected)
+                                }
+                            },
+                        )
+                    } else {
+                        pizza.copy(
+                            pizzaIngredients = pizza.pizzaIngredients.mapIndexed { index, ingredient ->
+                                ingredient.copy(isSelected = ingredient.isSelected)
+                            },
+                        )
+                    }
+                },
+                currentPage = pizzaPosition,
+            )
+        }
+    }
+
+    override fun onChangePizzaSize(position: Int, size: Float) {
+        val currentState = _state.value
+        val updatedPizza = currentState.pizzaBreads.mapIndexed { index, pizzaUiState ->
+            if (index == position) {
+                pizzaUiState.copy(defaultSize = size)
+            } else {
+                pizzaUiState
+            }
+        }
+        val updatedState = currentState.copy(pizzaBreads = updatedPizza)
+        _state.value = updatedState
+    }
+}
